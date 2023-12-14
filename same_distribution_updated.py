efd039d208deeca09c1d70ad96291e7fa7721de5
pabloralves <pabloralves@protonmail.com>
2023-12-14 22:14:52 +0100

    Add Kolmogorov-Smirnov test and test 280 size city

--- same_distribution_updated.py
+++ same_distribution_updated.py
@@ -33,18 +33,19 @@ for i in iterations:
         quadratic_cool = results[4]['data'][2]['scores']
 
         # 3. Perform stat testing
+        
         # 3.1 Kruskal-Wallis H-test
         _, p_value = sp.stats.kruskal(logarithmic_cool, linear_cool,arithmetic_geometric_cool,exponential_cool,quadratic_cool)
+        
         SAME_DISTRIBUTION = p_value > SIGNIFICANCE
-
         pass_string = 'PASS' if SAME_DISTRIBUTION else 'FAIL'
         print(pass_string,'Kruskal-Wallis H-test:','(pval='+str(round(p_value,4))+')')
 
         # 3.2 K-sample Anderson-Darling test
         samples = [logarithmic_cool, linear_cool,arithmetic_geometric_cool,exponential_cool,quadratic_cool]
         _, _, p_value = sp.stats.anderson_ksamp(samples)
-        SAME_DISTRIBUTION = p_value > SIGNIFICANCE
 
+        SAME_DISTRIBUTION = p_value > SIGNIFICANCE
         pass_string = 'PASS' if SAME_DISTRIBUTION else 'FAIL'
         print(pass_string,'K-sample Anderson-Darling test:','(pval='+str(round(p_value,4))+')')
 
@@ -53,9 +54,26 @@ for i in iterations:
         _, pvalue_2 = sp.stats.ks_2samp(linear_cool, arithmetic_geometric_cool)
         _, pvalue_3 = sp.stats.ks_2samp(arithmetic_geometric_cool, exponential_cool)
         _, pvalue_4 = sp.stats.ks_2samp(exponential_cool, quadratic_cool)
-        SAME_DISTRIBUTION = pvalue_1 > SIGNIFICANCE and pvalue_2 > SIGNIFICANCE and pvalue_3 > SIGNIFICANCE and pvalue_4 > SIGNIFICANCE
         
+        SAME_DISTRIBUTION = pvalue_1 > SIGNIFICANCE and pvalue_2 > SIGNIFICANCE and pvalue_3 > SIGNIFICANCE and pvalue_4 > SIGNIFICANCE
         pass_string = 'PASS' if SAME_DISTRIBUTION else 'FAIL'
         print(pass_string,'Kolmogorov-Smirnov test:')
         print('p_values:',pvalue_1,pvalue_2,pvalue_3,pvalue_4)
         print()
+
+
+# EXTRA: Plot points for insight
+with open('a280_results_10000.txt') as f:
+    results = eval(f.read())
+    logarithmic_cool = results[0]['data'][2]['scores']
+    linear_cool = results[1]['data'][2]['scores']
+    arithmetic_geometric_cool = results[2]['data'][2]['scores']
+    exponential_cool = results[3]['data'][2]['scores']
+    quadratic_cool = results[4]['data'][2]['scores']
+    
+    plt.scatter(np.ones(len(logarithmic_cool))*0,logarithmic_cool)
+    plt.scatter(np.ones(len(linear_cool))*1,linear_cool)
+    plt.scatter(np.ones(len(arithmetic_geometric_cool))*2,arithmetic_geometric_cool)
+    plt.scatter(np.ones(len(exponential_cool))*3,exponential_cool)
+    plt.scatter(np.ones(len(quadratic_cool))*4,quadratic_cool)
+    plt.show()
